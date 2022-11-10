dd2e275454fa4c00455c104583e73ab2f78d24ae
OWinnie <73723093+WinnieOdokara@users.noreply.github.com>
2022-11-10 02:07:04 +0000

    Added comments

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+# Install Selenium
+
+# Open the "replit.nix" file on hiden files. 
+  # On line 4 add: pkgs.chromium
+  # On line 5 add: pkgs.cromedriver
+
 from selenium import webdriver
 
 def get_driver():
@@ -5,18 +11,21 @@ def get_driver():
   options = webdriver.ChromeOptions()
   options.add_argument("disable-infobars")
   options.add_argument("start-maximized")
-  options.add_argument("disable-dev-shm-usage")
-  options.add_argument("no-sandbox")
+  options.add_argument("disable-dev-shm-usage") # avoid issue when running in Linux
+  options.add_argument("no-sandbox") # Disable sandbox in the webpage
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
-  options.add_argument("disable-blink-features=AutomationControlled")
-  
+  options.add_argument("disable-blink-features=AutomationControlled") #
+
   driver = webdriver.Chrome(options=options)
   driver.get("https://automated.pythonanywhere.com/")
   return driver
 
+
 def main():
   driver = get_driver()
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+  element = driver.find_element(by="xpath",
+                                value="/html/body/div[1]/div/h1[1]")
   return element.text
+
 
 print(main())
